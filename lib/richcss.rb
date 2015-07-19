33ea468bbbf5ea5c182a1c6de7ab5e1b36e0ecce
bill-x <billxu93@gmail.com>
2015-07-18 21:42:04 -0400

    generate template folders/files

--- lib/richcss.rb
+++ lib/richcss.rb
@@ -1,10 +1,7 @@
-require "richcss/version"
+require 'richcss/version'
 
 module Richcss
-  class Richcss
-    def init()
-      Richcss::Generators::Template.start('box')
-      Richcss::Generators::Template.start('elements')
-      Richcss::Generators::Template.start('parts')
-    end
+  class << self
+    # TODO
+  end
 end
--- spec/richcss_spec.rb
+++ spec/richcss_spec.rb
@@ -1,11 +1,7 @@
-require 'spec_helper'
+require 'richcss'
 
 describe Richcss do
   it 'has a version number' do
     expect(Richcss::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
   end
 end
