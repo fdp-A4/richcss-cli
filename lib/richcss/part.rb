a53f2f92f415587704deb1266e292ec96488b048
nicholastlo <nicholastclo@gmail.com>
2016-03-14 15:16:19 -0400

    fixed downloadlink

--- lib/richcss/part.rb
+++ lib/richcss/part.rb
@@ -49,7 +49,7 @@ module Richcss
           homepage = homepage.split("\/")
           repo_owner = homepage[0]
           repo_name = homepage[1]
-          jsonResponse = JSON.parse(Net::HTTP.get(URI("https://api.github.com/repos/#{repo_owner}/#{repo_name}/releases/tags/v#{body["version"]}")))
+          jsonResponse = JSON.parse(Net::HTTP.get(URI("https://api.github.com/repos/#{repo_owner}/#{repo_name}/releases/tags/#{body["version"]}")))
           downloadLink = jsonResponse["zipball_url"]
           install(part_name, body["version"], downloadLink)
         else
